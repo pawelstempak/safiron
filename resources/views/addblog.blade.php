d944c84a804b26aa3acbe7f2cda99b8971a33dce
Paweł Stempak <pawelstempak@gmail.com>
2023-07-21 21:41:07 +0200

    modified:   resources/views/addblog.blade.php 	modified:   resources/views/blog.blade.php 	modified:   resources/views/editblog.blade.php

--- resources/views/addblog.blade.php
+++ resources/views/addblog.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Dodaj nowy wpis dziennika
+        Dodaj nowy wpis w galerii
         </h2>
     </x-slot>
 
--- resources/views/editblog.blade.php
+++ resources/views/editblog.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Dodaj nowy wpis dziennika
+            Edytuj wpisy w galerii
         </h2>
     </x-slot>
 
