50cff795368e1958e079fe88be82f74e982dcc65
Paweł Stempak <pawelstempak@gmail.com>
2023-07-22 12:48:24 +0200

    modified:   resources/views/frontend/log.blade.php

--- resources/views/frontend/log.blade.php
+++ resources/views/frontend/log.blade.php
@@ -4,11 +4,11 @@
 
         <section class="pt-10 pb-10 lg:pt-20 lg:pb-20 px-5">
             <div class="md:container mx-auto">
-                <h1 class="p-5 text-primary mb-5 block text-6xl font-exo">GALERIA</h1>
+                <h1 class="md:p-5 text-primary mb-5 block text-6xl font-exo sm:ml-32 md:ml-0">GALERIA</h1>
                 <div class="-mx-4 flex flex-wrap">
                     @foreach($loglist as $log)
                     <div class="w-full px-4 md:w-1/2 lg:w-1/3">
-                        <div class="mx-auto mb-10 max-w-[450px] bg-slate-300 p-5 rounded-lg shadow-md">
+                        <div class="mx-auto mb-10 sm:max-w-[450px] md:max-w-[450px] bg-slate-300 p-5 rounded-lg shadow-md">
                             <div class="relative mb-2 h-52 overflow-hidden rounded">
                                 <a href="log/{{$log->blogs_id}}"><img src="{{Storage::url('blog/'.$log->filename)}}" alt="image" class="object-cover w-full h-full" /></a>
                                 <div class="shadow-lg absolute bottom-2 left-2 bg-white  inline-block rounded py-1 px-4 text-center text-xs font-semibold leading-loose text-primary">
